de8be4135b3a1a46c5ab45e8fdf0c3bd5b16e74a
muriuki54 <muriukicharles54@gmail.com>
2023-06-22 23:07:26 +0300

    Log connection error

--- db.php
+++ db.php
@@ -27,7 +27,10 @@ try {
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo json_encode(array("message", "Connected successfully to " . $environment));
+    // die();
 } catch (PDOException  $e) {
+    http_response_code(500);
     echo json_encode(array("message", "Connection failed ". $e->getMessage()));
+    logErrorToFile($e->getMessage());
     die();
 }
