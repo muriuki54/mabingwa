ddb772a1e0c5f752dd842ca1439570e1f40e0353
muriuki54 <muriukicharles54@gmail.com>
2023-08-05 22:25:49 +0300

    Add golden boots count to DB

--- api.php
+++ api.php
@@ -60,7 +60,7 @@ function handleData() {
 
         // if(! $new_stats_file) echo $php_errormsg;
         // Write headers to CSV file
-        fputcsv($new_stats_file, array("Name", "GF", "GA", "Played", "Won"));
+        fputcsv($new_stats_file, array("Name", "GF", "GA", "Played", "Won", "Golden Boot"));
 
         // Insert to DB
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -72,24 +72,26 @@ function handleData() {
                 $gf = 0;
                 $played = 0;
                 $won = 0;
+                $golden_boot = 0;
             } else {
                 $ga = $stat->ga;
                 $gf = $stat->gf;
                 $played = 1;
                 $won = $stat->won == false ? 0 : 1;
+                $golden_boot = $stat->goldenBoot == false ? 0: 1;
             }
             
             if($_SERVER["HTTP_HOST"] === "localhost") {
                 // $sql = "UPDATE players SET ga = 0, gf = 0, played = 0, won = 0 WHERE id = $id";
             }
             
-            $sql = "UPDATE players SET ga = ga + $ga, gf = gf + $gf, played = played + $played, won = won + $won WHERE id = $id";
+            $sql = "UPDATE players SET ga = ga + $ga, gf = gf + $gf, played = played + $played, won = won + $won, goldenboot = goldenboot + $golden_boot WHERE id = $id";
             $stmt = $conn->prepare($sql);
             $stmt->execute();
 
             // Write data to CSV file for redudancy
             $statArray = (array)$stat;
-            fputcsv($new_stats_file, [$statArray["name"], $statArray["gf"], $statArray["ga"], $statArray["played"] ? "Yes" : "No",  $statArray["won"] ? "Yes" : "No"]);
+            fputcsv($new_stats_file, [$statArray["name"], $statArray["gf"], $statArray["ga"], $statArray["played"] ? "Yes" : "No",  $statArray["won"] ? "Yes" : "No", $statArray["goldenBoot"] ? "Yes" : "No"]);
         }
 
         // Close stats file
