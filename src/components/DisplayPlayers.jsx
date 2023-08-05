5e70c265b738a26b043b7d82792b66a362f0cbed
muriuki54 <muriukicharles54@gmail.com>
2023-08-05 22:28:05 +0300

    Add new golden boot tab

--- src/components/DisplayPlayers.jsx
+++ src/components/DisplayPlayers.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef, useEffect, useRef, useState } from 'react'
-import HallOfFame from './HallOfFame'
+import TournamentWinnersHallOfFame from './TournamentWinnersHallOfFame'
 import VisualDisplay from './VisualDisplay';
+import GoldenBallsHallOfFame from './GoldenBallsHallOfFame';
 
 const DisplayPlayers = forwardRef(function(props, ref) {
 
@@ -8,13 +9,14 @@ const DisplayPlayers = forwardRef(function(props, ref) {
 
   let [error, setError] = useState(false);
   let [players, setPlayers] = useState([]);
-  let [hallOfFamers, sethallOfFamers] = useState([]);
+  let [tournamentsHallOfFamers, settournamentsHallOfFamers] = useState([]);
+  let [goldenBootsHallOfFamers, setgoldenBootsHallOfFamers] = useState([]);
+  let [allTimeGames, setallTimeGames] = useState(1);
+  let [hallofFameTab, sethallofFameTab] = useState(0);
 
 
   useEffect(() => {
     async function fetchPlayers() {
-      // let apiUrl = "https://leaderboard.image-editor-online.com/api.php?action=fetchplayers";
-      // let apiUrl = "https://localhost/mabingwa/api.php?action=fetchplayers";
       let apiUrl = window.location.host.indexOf("localhost") > -1 ? "https://localhost/mabingwa/api.php?action=fetchplayers" : "https://leaderboard.image-editor-online.com/api.php?action=fetchplayers";
       let response, data;
       try {
@@ -26,29 +28,78 @@ const DisplayPlayers = forwardRef(function(props, ref) {
         console.log(response)
         data = await response.json();
         // data = await response.text();
+
+        if(data && data.players) {
+          const mostParticipated = data.players.reduce((maxPlayed, currentObj) => {
+            if (Number(currentObj.played) > Number(maxPlayed.played)) {
+              return currentObj;
+            } else {
+              return maxPlayed;
+            }
+          }, data.players[0]);
+          setallTimeGames(prevState => Number(mostParticipated.played));
+  
+          let validatedPlayersArray = [];
+          let MIAPlayersArray = [];
+  
+          data.players.map(function(player) {
+            validateParticipation(Number(mostParticipated.played), player.played) ? validatedPlayersArray.push(player) : MIAPlayersArray.push(player);
+          })
+  
+          setPlayers(function(prevState) {
+            return [...prevState, ...validatedPlayersArray, ...MIAPlayersArray]
+          })
+
+          settournamentsHallOfFamers(function(prevState) {
+            return[...prevState, ...validatedPlayersArray.slice(0,3)]
+          })
+
+          setgoldenBootsHallOfFamers(function(prevState) {
+            let sortedGoldenBootsArr = validatedPlayersArray.sort((a, b) => Number(b.goldenboot) - Number(a.goldenboot));
+            // sortedGoldenBootsArr.filter(el => Number(el.goldenboot) > 0)
+            console.log(sortedGoldenBootsArr)
+            return[...prevState, ...sortedGoldenBootsArr.filter(el => Number(el.goldenboot) > 0).slice(0,3)]
+          })
+  
+        } else {
+          setError(true);
+        }
       } catch(e) {
         console.log(e)
       }
-  
-      if(data && data.players) {
-        setPlayers(function(prevState) {
-          return [...prevState, ...data.players]
-        })
-        sethallOfFamers(function(prevState) {
-          return[...prevState, ...data.players.slice(0,3)]
-        })
-      } else {
-        setError(true);
-      }
     }
 
-    fetchPlayers(); // Fetch players from the DB
-    // console.log("called");
+    fetchPlayers(); // Fetch players from the API
   }, [])
+
+  function validateParticipation(t, p) {
+    let total = Number(t),
+        participated = Number(p);
+
+    let percentage = Math.round((participated / total) * 100);
+    return percentage > 50; // check if palyer participated in atleast a half of the total played tournaments
+  }
+
+  function changeHallofFameTab(e) {
+    e.target.checked ? sethallofFameTab(1) : sethallofFameTab(0);
+  }
   
   return (
     <>
-    <HallOfFame hallOfFamers={hallOfFamers} error={error} />
+    <div className="hall-of-fame">
+      <form action="" id="hall-of-famers-toggle">
+        <p>Tournaments</p>
+        <input type="checkbox" onChange={e => changeHallofFameTab(e)} name="hall-of-famers-toggle-button" id="hall-of-famers-toggle-button" />
+        <label htmlFor="hall-of-famers-toggle-button" title="Click me"></label>
+        <p>Golden Boots</p>
+      </form>
+
+    {hallofFameTab === 0 ? 
+      <TournamentWinnersHallOfFame tournamentsHallOfFamers={tournamentsHallOfFamers} error={error} />
+      :
+      <GoldenBallsHallOfFame goldenbootsHallOfFamers={goldenBootsHallOfFamers} error={error} />
+    }
+    </div>
 
     <div className="players-table" ref={ref}>
       <div className="container">
@@ -57,6 +108,9 @@ const DisplayPlayers = forwardRef(function(props, ref) {
               <li>Most tournaments won</li>
               <li>Goal Difference</li>
             </ol>
+
+            <br />
+            <strong>* Players with less than 50% participation are labeled as Missing In Action (MIA)</strong>
             <hr />
             <div className="players-titles">
               <p><span className="display-lg">Rank</span><span className="display-sm">#</span></p>
@@ -72,8 +126,8 @@ const DisplayPlayers = forwardRef(function(props, ref) {
             {
               players.map(function(player, index) {
                 return (
-                  <div className={index === 0 ? "player player-first" : "player"} key={index}>
-                    <p className="player-position">#{index + 1}</p>
+                  <div className={index === 0 ? `player player-first ${validateParticipation(allTimeGames, player.played) ? '' : 'mia'}` : `player ${validateParticipation(allTimeGames, player.played) ? '' : 'mia'}`} key={index}>
+                    <p className={validateParticipation(allTimeGames, player.played) ? `player-position` : `player-position player-position-in-question`}>{validateParticipation(allTimeGames, player.played) ? `#${index + 1}` : "MIA"}</p>
                     <p className="player-name">{player.name}</p>
                     <p className="wins">{player.played}</p>
                     <p className="wins">{player.won}</p>
